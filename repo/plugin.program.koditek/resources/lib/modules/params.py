0ad294a207b45e22bce6d192401bdcf744d4cabb
SubTek <760675+SubTek@users.noreply.github.com>
2024-09-13 20:46:55 -0400

    commit

---
+++ repo/plugin.program.koditek/resources/lib/modules/params.py
@@ -0,0 +1,56 @@
+from urllib.parse import parse_qsl
+
+class Params:
+    def __init__(self, paramstring):
+        self.params = dict(parse_qsl(paramstring))
+
+    def get_params(self):
+        return self.params
+
+    def get_name(self):
+        try:
+            return self.params['name']
+        except (KeyError, TypeError):
+            return None
+
+    def get_name2(self):
+        try:
+            return self.params['name2']
+        except (KeyError, TypeError):
+            return None
+
+    def get_version(self):
+        try:
+            return self.params['version']
+        except (KeyError, TypeError):
+            return None
+
+    def get_url(self):
+        try:
+            return self.params['url']
+        except (KeyError, TypeError):
+            return None
+
+    def get_mode(self):
+        try:
+            return int(self.params['mode'])
+        except (KeyError, TypeError):
+            return None
+
+    def get_icon(self):
+        try:
+            return self.params['icon']
+        except (KeyError, TypeError):
+            return None
+
+    def get_fanart(self):
+        try:
+            return self.params['fanart']
+        except (KeyError, TypeError):
+            return None
+
+    def get_description(self):
+        try:
+            return self.params['description']
+        except (KeyError, TypeError):
+            return None
---
+++ repo/plugin.program.koditek/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/SubTek/koditek/master/builds/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url = 'https://raw.githubusercontent.com/SubTek/koditek/master/builds/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes = ['plugin.video.whatever']
